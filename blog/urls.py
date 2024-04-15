937775eba37746bfd0fa6ddab442ebfad5d03a8a
alinorwa <alialrubay399@gmail.com>
2024-04-15 14:23:50 +0200

    'comment'

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,6 @@ from .views import *
 
 urlpatterns = [
     path('',home_page , name='home_Page'),
+    path('post/<int:pk>/',detail_post , name='detail_post'),
+    path('post/<int:post_id>/comment/', add_comment, name='add_comment'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render , get_object_or_404
+from django.http import JsonResponse
 from django.db.models import Q
 from .models import Post
-
+from .forms import CommentForm
 # Create your views here.
 
 def home_page(request):
@@ -13,3 +14,20 @@ def home_page(request):
         posts = Post.objects.all()
     context = {'posts':posts}            
     return render(request ,'blog/home_page.html' , context)
+
+def detail_post(request ,pk):
+    post = get_object_or_404(Post , pk=pk)
+    return render(request , 'blog/detail.html',{'post':post})
+
+
+def add_comment(request, post_id):
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            post = Post.objects.get(pk=post_id)
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.author = request.user
+            comment.save()
+            return JsonResponse({'success': True})  # Send JSON response indicating success
+    return JsonResponse({'success': False, 'errors': form.errors})
